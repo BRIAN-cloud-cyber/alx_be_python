4920037d4983bde8b7b991d8e882c80cdbcdf52a
BRIAN-cloud-cyber <omondiomimo@gmail.com>
2025-11-27 05:49:48 +0300

    lists

---
+++ fns_and_dns/shopping_list_manager.py
@@ -0,0 +1,32 @@
+ 
+
+def dislpay_menu():
+    print("Shopping List Manager")
+    print("1: Add Item" )
+    print("2: Remove Item")
+    print("3: View List")
+    print("4: EXit ")
+
+
+def main ():
+    shopping_list=[]
+    while True:
+        dislpay_menu()
+        choice=input("Enter your choice:  ")
+        if choice =='1':
+            item_add=input("What do you want to add to the shopping list: ")
+            shopping_list.append(item_add)
+        
+        elif choice =='2':
+            item_remove=input("What do you want to remove from the shopping list: ")
+            shopping_list.remove(item_remove)
+        elif choice =='3':
+            print(shopping_list)
+
+        elif choice=='4':
+            print("Goodbye!")
+            break 
+        else:
+            print("Invalid choice.Please try again")
+if __name__=="__main__":
+    main()
