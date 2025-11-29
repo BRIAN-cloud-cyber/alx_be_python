aeec41983574a4073ebf654ec949abb8c8115eed
BRIAN-cloud-cyber <omondiomimo@gmail.com>
2025-11-29 10:27:36 +0300

    BankAccount

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+class BankAccount:
+    
+    def __init__(self,account_balance=0):
+        self.balance=account_balance
+        
+    def deposit(self,amount):
+        self.account_balance +=amount 
+
+    def withdraw (self,amount):
+         if amount <=self.account_balance:
+             self.account_balance -=amount
+             return True 
+         else:
+             return False 
+         
+    def display_balance(self):
+        print(f"Current balance:{self.account_balance}")
+
+    
+
+user1=BankAccount()
+
